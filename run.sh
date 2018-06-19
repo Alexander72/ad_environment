b5dbd2719b7db1ea7d222c614a6dc84e980a6a0b
Alexander Volkov <alexandr.volkov@cyberhull.com>
2018-06-19 14:24:45 +0300

    setup phpstorm debugger

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose up -d
+HOST_IP=`ip -4 addr show scope global dev docker0 | grep inet | awk '{print \$2}' | cut -d / -f 1`
+export HOST_IP=$HOST_IP && docker-compose up -d
